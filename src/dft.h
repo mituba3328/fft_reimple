6c0b86153026c74bdd67afe6e45914a2adc22e8c
mituba328 <kitamagotake1220@gmail.com>
2026-05-13 14:37:15 +0900

    made callable the "dft.c", "fft.c"

---
+++ src/dft.h
@@ -0,0 +1,4 @@
+#include <stddef.h>
+#include <complex.h>
+
+void dft(double complex* out, const double complex* in, size_t N);
--- src/fft.c
+++ src/fft.c
@@ -1,29 +1,7 @@
 #define _GNU_SOURCE
-#include <stddef.h>
 #include <stdio.h>
-#include <complex.h>
 #include <math.h>
-
-#define Sample_N 1024
-
-void fft(double complex* out, const double complex* in, size_t N);
-void bit_reverse(double complex *x, size_t N);
-void target_func(double complex *out, size_t N);
-
-int main(void) {
-    double complex samples[Sample_N];
-    double complex freq_domain[Sample_N];
-    target_func(samples, Sample_N);
-
-    fft(freq_domain, samples, Sample_N);
-    bit_reverse(freq_domain, Sample_N);
-
-    printf("k, F(k)\n");
-    for (size_t i = 0; i < Sample_N; i++) {
-        printf("%zu, %lf\n", i, cabs(freq_domain[i]));
-    }
-    return 0;
-}
+#include "fft.h"
 
 void fft(double complex* out, const double complex* in, size_t N) {
     if (N == 2) {
@@ -58,11 +36,3 @@ void bit_reverse(double complex *x, size_t N) {
         j += m;
     }
 }
-
-void target_func(double complex *out, size_t N) {
-    for (size_t n = 0; n < N; n++) {
-        out[n] = csin((2 * M_PI * n / (double)N) * 3) + csin((2 * M_PI * n / (double)N) * 6);
-    }
-    return;
-}
-
